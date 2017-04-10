25402d81960825c6067dccb7d996f0b8193a959b
zhzjwd <zhzjwd@users.noreply.github.com>
2017-04-10 23:22:29 +0800

    curl获取GV

--- GoogleVoice/get_gv_loop_ubuntu.sh
+++ GoogleVoice/get_gv_loop_ubuntu.sh
@@ -1,3 +1,4 @@
+#记录，防忘
 #脚本写入到gv.sh文件，curl更换为自己的
 cat << EOF > gv.sh
 for (( i=1; i>0; i++ ))
@@ -8,22 +9,22 @@ done
 EOF
 
 #nohup命令(即no hang up)脱离终端后台运行shell
-nohup bash ./gv.sh &   #日志会输出到当前目录的nohup.out文件
+nohup bash ./gv.sh &   #命令输出会写入到到当前目录的nohup.out文件
 #当shell中提示了nohup成功后还需要按终端上键盘任意键退回到shell输入命令窗口
 #然后通过在shell中输入exit来退出终端
 #否则会断掉该命令所对应的session，导致nohup对应的进程被通知需要一起shutdown
 #使用 jobs  查看任务 
 #使用 fg %n 关闭任务
 
-#安装远程桌面已便获得本机的curl(非必须)，使用Windows的mstsc连接
-#因centos和Debian的vnc有问题无法解决所以又折腾的Ubuntu
+#安装远程桌面以便获得本机的curl(非必须)，使用Windows的mstsc连接
+#因centos和Debian的vnc遇到问题没解决所以又折腾的Ubuntu
 apt-get update
 apt-get -y install xrdp
 apt-get update
 apt-get -y install vnc4server
 apt-get update
 apt-get -y install xubuntu-desktop
-#修改配置文件避免桌面间不兼容导致的无画面
+#修改配置文件避免桌面间不兼容导致的全白点画面
 echo "xfce4-session" > ~/.xsession
 service xrdp restart
 #安装Chrome
@@ -33,3 +34,5 @@ sudo dpkg -i google-chrome-stable_current_i386.deb
 #64位
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+#以root用户运行Chrome需要在启动命令后加入
+--user-data-dir --no-sandbox
